dd06e9bbf84ed081d7528ffe24b9b9683952e276
cildhdi <miles_yang@foxmail.com>
2018-12-22 15:22:22 +0800

    add args

--- CmdArguments/CmdArguments.cpp
+++ CmdArguments/CmdArguments.cpp
@@ -3,13 +3,43 @@
 namespace cl
 {
 
-CmdArguments::CmdArguments()
+
+CmdArguments::CmdArguments(int argc, char ** argv)
 {
+    _app_name = argv[0];
+    _args.reserve(argc - 1);
+    for (std::size_t i = 1; i < argc; i++)
+        _args.emplace_back(argv[i]);
 }
 
-
-CmdArguments::~CmdArguments()
+bool CmdArguments::exist(const std::string & arg_name) const
 {
+    return std::find_if(_args.begin(), _args.end(), [&](const cl::CmdArgument& arg) 
+    {
+        return arg.name() == arg_name;
+    }) == _args.end();
+}
+
+std::string CmdArguments::value(const std::string & arg_name) const
+{
+    auto it = std::find_if(_args.begin(), _args.end(), [&](const cl::CmdArgument& arg)
+    {
+        return arg.name() == arg_name;
+    });
+    if (it == _args.end())
+        throw std::exception("no such argument");
+    else
+        return it->value();
+}
+
+CmdArguments::Arguments::iterator CmdArguments::begin()
+{
+    return _args.begin();
+}
+
+CmdArguments::Arguments::iterator CmdArguments::end()
+{
+    return _args.end();
 }
 
 }
--- CmdArguments/CmdArguments.h
+++ CmdArguments/CmdArguments.h
@@ -1,13 +1,35 @@
 #pragma once
 
+#include "CmdArgument.h"
+#include <string>
+#include <vector>
+#include <algorithm>
 namespace cl
 {
 
 class CmdArguments
 {
 public:
-    CmdArguments();
-    ~CmdArguments();
+    using Arguments = std::vector<cl::CmdArgument>;
+private:
+    std::string _app_name;
+    Arguments _args;
+public:
+    CmdArguments(int argc, char ** argv);
+    bool exist(const std::string& arg_name) const;
+    std::string value(const std::string& arg_name) const;
+    Arguments::iterator begin();
+    Arguments::iterator end();
 };
+
+inline cl::CmdArguments::Arguments::iterator begin(CmdArguments& args)
+{
+    return args.begin();
+}
+
+inline cl::CmdArguments::Arguments::iterator end(CmdArguments& args)
+{
+    return args.end();
+}
 
 }
